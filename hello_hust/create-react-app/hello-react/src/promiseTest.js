2c8998ae12da81649833668567d9e6968f8585c1
Lyihong626 <2589705098@qq.com>
2026-07-14 10:52:39 +0800

    new

---
+++ hello_hust/create-react-app/hello-react/src/promiseTest.js
@@ -0,0 +1,18 @@
+//回调
+const fs = require('fs');
+// fs.readFile('./src/content.txt',(err,data)=>{
+//     if(err) throw err;
+//     console.log(data.toString());
+// }) 
+//promise形式
+let p = new Promise((resolve, reject) => {
+    fs.readFile('./src/content.txt', (err, data) => {
+        if (err) reject(err);
+        resolve(data);
+    })
+})
+p.then(value=>{
+    console.log(value.toString());
+},reason=>{
+    console.log(reason);
+})
